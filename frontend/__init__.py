f86eec094d55a5d2769bc004a89bdcc801296032
Luciano Filho <lvgalvaofilho@gmail.com>
2023-12-15 17:31:31 -0300

    frontend

---
+++ frontend/__init__.py
@@ -0,0 +1,5 @@
+import sys
+from pathlib import Path
+
+# Adiciona o diretório pai ao PYTHONPATH
+sys.path.append(str(Path(__file__).parent.parent))
--- src/tools/aws/client.py
+++ src/tools/aws/client.py
@@ -4,20 +4,24 @@ from pyathena import connect
 import sys
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Suponha que você tenha uma variável de ambiente chamada "MINHA_VARIAVEL"
 # Você pode acessar seu valor usando a função os.environ.get()
-
 
 
 class S3Client:
 
     def __init__(self):
+        
         self._envs = {
             "aws_access_key_id": os.environ.get("AWS_ACCESS_KEY_ID"),
             "aws_secret_access_key": os.environ.get("AWS_SECRET_ACCESS_KEY"),
             "region_name": os.environ.get("AWS_REGION", "us-west-1"),  # Usando um valor padrão se a variável não estiver definida
             "s3_bucket": os.environ.get("S3_BUCKET_NAME"),
-            "datalake" : os.environ.get("DELTA_LAKE_S3_PATH")
+            "datalake" : os.environ.get("CATALOGO_S3_PATH")
         }
 
         for var in self._envs:
